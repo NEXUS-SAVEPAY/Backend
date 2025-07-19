a6d145e1a1c76bb97cd6b1b90135248d37dd6411
wnd01jun <changjun157@naver.com>
2025-07-19 21:57:24 +0900

    Feat(): Project initial commit

---
+++ src/main/java/savepay/savepay/SavepayApplication.java
@@ -0,0 +1,13 @@
+package savepay.savepay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavepayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavepayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/savepay/savepay/SavepayApplicationTests.java
@@ -0,0 +1,13 @@
+package savepay.savepay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavepayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
