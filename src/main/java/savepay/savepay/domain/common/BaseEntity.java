9b55d86e2e2301bb41b0ba9b7aa01c264dc4be4c
wnd01jun <changjun157@naver.com>
2025-07-23 23:51:46 +0900

    feat(#1): BaseEntity 추가

---
+++ src/main/java/savepay/savepay/domain/common/BaseEntity.java
@@ -0,0 +1,36 @@
+package savepay.savepay.domain.common;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+/*
+    모든 Entity의 기본이 되는 속성들을 갖고 있는 BaseEntity 입니다.
+ */
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass // 공통 매핑 정보 지정
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@SuperBuilder
+@Getter
+@Setter
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
