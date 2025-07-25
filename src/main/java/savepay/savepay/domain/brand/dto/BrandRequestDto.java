5f70a8d2e0d5a9ff0b7bd018313a08fab9ceaf0b
jihoo2002 <yolohoo@naver.com>
2025-07-25 13:24:56 +0900

    Feat(#7) : Brand DTO 추가

---
+++ src/main/java/savepay/savepay/domain/brand/dto/BrandRequestDto.java
@@ -0,0 +1,12 @@
+package savepay.savepay.domain.brand.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class BrandRequestDto {
+
+    @Getter
+    public static class BrandNameRequestDto {
+        String name;
+    }
+}
---
+++ src/main/java/savepay/savepay/domain/brand/dto/BrandResponseDto.java
@@ -0,0 +1,4 @@
+package savepay.savepay.domain.brand.dto;
+
+public class BrandResponseDto {
+}
