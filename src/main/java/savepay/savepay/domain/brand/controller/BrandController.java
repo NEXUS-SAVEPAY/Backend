961ab19f6b8e1e085ede765643139aff0997547f
jihoo2002 <yolohoo@naver.com>
2025-07-25 23:24:34 +0900

    Feat(#7) : Swagger 아노테이션 추가

--- src/main/java/savepay/savepay/domain/brand/controller/BrandController.java
+++ src/main/java/savepay/savepay/domain/brand/controller/BrandController.java
@@ -1,10 +1,9 @@
 package savepay.savepay.domain.brand.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import savepay.savepay.domain.brand.dto.BrandRequestDto;
 import savepay.savepay.domain.brand.dto.BrandResponseDto;
 import savepay.savepay.domain.brand.service.BrandService;
@@ -13,12 +12,14 @@ import savepay.savepay.global.ApiResponse;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/brand")
+@Tag(name = "브랜드 API", description = "브랜드 검색을 통해 브랜드 정보 조회 API")
 public class BrandController {
 
     private final BrandService brandService;
 
-    @GetMapping("/search")
-    public ApiResponse<BrandResponseDto.BrandInfoDto> searchBrand(BrandRequestDto.BrandNameRequestDto request) {
+    @PostMapping("/search")
+    @Operation(summary = "브랜드 검색 API", description = "사용자가 브랜드 검색할 때 조회하는 메서드")
+    public ApiResponse<BrandResponseDto.BrandInfoDto> searchBrand(@RequestBody BrandRequestDto.BrandNameRequestDto request) {
         return ApiResponse.onSuccess(brandService.searchBrand(request));
     }
 }
