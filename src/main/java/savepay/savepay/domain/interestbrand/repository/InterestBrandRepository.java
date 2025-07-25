a0ab9cfcde2fd7922795a42f27277a8ddd2e6910
jihoo2002 <yolohoo@naver.com>
2025-07-25 15:32:22 +0900

    Feat(#9) : InterestBrandRepository 추가

---
+++ src/main/java/savepay/savepay/domain/interestbrand/repository/InterestBrandRepository.java
@@ -0,0 +1,7 @@
+package savepay.savepay.domain.interestbrand.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import savepay.savepay.domain.interestbrand.entity.InterestBrand;
+
+public interface InterestBrandRepository extends JpaRepository<InterestBrand, Long> {
+}
