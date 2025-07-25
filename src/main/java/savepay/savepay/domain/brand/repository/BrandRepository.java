98cd485151f37e92391f23a00acfb1443249bd4c
jihoo2002 <yolohoo@naver.com>
2025-07-25 13:24:44 +0900

    Feat(#7) : BrandRepository 추가

---
+++ src/main/java/savepay/savepay/domain/brand/repository/BrandRepository.java
@@ -0,0 +1,10 @@
+package savepay.savepay.domain.brand.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import savepay.savepay.domain.brand.entity.Brand;
+
+import java.util.Optional;
+
+public interface BrandRepository extends CrudRepository<Brand, Long> {
+    Optional<Brand> findByName(String name);
+}
