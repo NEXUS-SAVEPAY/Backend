ff14e7fa5b3ec1f08ca21407e8d84fc60f660505
Hyun-38 <155708154+Hyun-38@users.noreply.github.com>
2025-07-26 11:39:08 +0900

    Feat(#10) : PaymentRequestDto 추가

---
+++ src/main/java/savepay/savepay/domain/payment/dto/PaymentRequestDto.java
@@ -0,0 +1,14 @@
+package savepay.savepay.domain.payment.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import savepay.savepay.domain.payment.entity.PaymentType;
+
+@Getter
+@NoArgsConstructor
+public class PaymentRequestDto {
+    private Long userId; // UserId
+    private Long cardId; // CardId
+    private int amount; // 결제 요청 시 결제하려는 금액
+    private PaymentType paymentType; // 결제 지불 방식
+}
