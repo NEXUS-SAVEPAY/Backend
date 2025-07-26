3aec067dce5d98e98c48ee863b0b17f576225064
wnd01jun <changjun157@naver.com>
2025-07-26 15:46:53 +0900

    fix(#8): TelecomGrade 잘못 입력된 값 수정

--- src/main/java/savepay/savepay/domain/usertelecom/entity/TelecomGrade.java
+++ src/main/java/savepay/savepay/domain/usertelecom/entity/TelecomGrade.java
@@ -3,5 +3,5 @@ package savepay.savepay.domain.usertelecom.entity;
 import lombok.ToString;
 
 public enum TelecomGrade {
-    SKT, GOLD, VIP, WHITE, VVIP, DIAMOND
+    SILVER, GOLD, VIP, WHITE, VVIP, DIAMOND
 }
