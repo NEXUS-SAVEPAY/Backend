7e827a757bdab3adf32061c9ed551ca56e34c027
Hyun-38 <155708154+Hyun-38@users.noreply.github.com>
2025-07-26 16:23:32 +0900

    Feat(#11) : PaymentResponseDto 변수 추가

--- src/main/java/savepay/savepay/domain/payment/dto/PaymentResponseDto.java
+++ src/main/java/savepay/savepay/domain/payment/dto/PaymentResponseDto.java
@@ -4,15 +4,19 @@ import lombok.Builder;
 import lombok.Getter;
 import savepay.savepay.domain.payment.entity.PaymentType;
 
-import java.time.LocalDateTime;
-
 @Getter
 @Builder
 
 public class PaymentResponseDto {
+    private Long id;
+    private Long userId;
     private Long cardId;
+
     private String company;
     private String cardName;
     private String cardImage;
+
+    private int amount;
+
     private PaymentType paymentType;
 }
