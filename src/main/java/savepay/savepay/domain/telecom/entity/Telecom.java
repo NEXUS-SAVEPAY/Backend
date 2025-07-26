65434a53d95082b9f3c97319061d48886b66c474
wnd01jun <changjun157@naver.com>
2025-07-26 16:29:24 +0900

    fix(#8): 필요 lombok 추가

--- src/main/java/savepay/savepay/domain/telecom/entity/Telecom.java
+++ src/main/java/savepay/savepay/domain/telecom/entity/Telecom.java
@@ -1,12 +1,16 @@
 package savepay.savepay.domain.telecom.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import savepay.savepay.domain.common.BaseEntity;
 import savepay.savepay.domain.user.entity.User;
 
 @Entity
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Telecom extends BaseEntity {
 
     @Enumerated(EnumType.STRING)
