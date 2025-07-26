32d8093d97cd636e69bdfc4b74099c5a1f25d7ad
wnd01jun <changjun157@naver.com>
2025-07-26 15:17:52 +0900

    feat(#8): Telecom 필드 변경

--- src/main/java/savepay/savepay/domain/telecom/entity/Telecom.java
+++ src/main/java/savepay/savepay/domain/telecom/entity/Telecom.java
@@ -1,17 +1,14 @@
 package savepay.savepay.domain.telecom.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
+import lombok.Getter;
 import savepay.savepay.domain.common.BaseEntity;
 import savepay.savepay.domain.user.entity.User;
 
 @Entity
+@Getter
 public class Telecom extends BaseEntity {
 
-    private String grade;
-
-    @OneToOne
-    @JoinColumn(name = "user_id")
-    private User user;
+    @Enumerated(EnumType.STRING)
+    private TelecomName telecomName;
 }
