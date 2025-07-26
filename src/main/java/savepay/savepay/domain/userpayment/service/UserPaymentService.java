b73a642ecfb6aa170639e34ef88b69ede20f2ace
Hyun-38 <155708154+Hyun-38@users.noreply.github.com>
2025-07-26 22:23:09 +0900

    Feat(#11) : ErrorStatus 결제수단 없는 요청 추가

--- src/main/java/savepay/savepay/domain/userpayment/service/UserPaymentService.java
+++ src/main/java/savepay/savepay/domain/userpayment/service/UserPaymentService.java
@@ -3,13 +3,15 @@ package savepay.savepay.domain.userpayment.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import savepay.savepay.domain.userpayment.converter.*;
+import savepay.savepay.domain.userpayment.converter.UserPaymentConverter;
 import savepay.savepay.domain.payment.entity.Payment;
 import savepay.savepay.domain.userpayment.dto.UserPaymentRequestDto;
 import savepay.savepay.domain.userpayment.dto.UserPaymentResponseDto;
 import savepay.savepay.domain.userpayment.entity.UserPayment;
 import savepay.savepay.domain.payment.repository.PaymentRepository;
 import savepay.savepay.domain.userpayment.repository.UserPaymentRepository;
+import savepay.savepay.global.code.status.ErrorStatus;
+import savepay.savepay.global.exception.GeneralException;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,10 +26,9 @@ public class UserPaymentService {
     @Transactional
     public UserPaymentResponseDto registerUserPayment(UserPaymentRequestDto dto) {
         Payment payment = paymentRepository.findById(dto.getPaymentId())
-                .orElseThrow(() -> new IllegalArgumentException("결제수단 없음"));
+                .orElseThrow(() -> new GeneralException(ErrorStatus.PAYMENT_NOT_FOUND));
 
         UserPayment userPayment = UserPayment.builder()
-                .userId(dto.getUserId())
                 .payment(payment)
                 .build();
         UserPayment saved = userPaymentRepository.save(userPayment);
--- src/main/java/savepay/savepay/global/code/status/ErrorStatus.java
+++ src/main/java/savepay/savepay/global/code/status/ErrorStatus.java
@@ -12,7 +12,9 @@ public enum ErrorStatus implements BaseErrorCode {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 에러, 관리자에게 문의 바랍니다."),
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON400", "잘못된 요청입니다."),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON401", "인증이 필요합니다."),
+    PAYMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "PAYMENT404", "결제수단을 찾을 수 없습니다."),
     FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다.");
+
 
     private final HttpStatus httpStatus;
     private final String code;
