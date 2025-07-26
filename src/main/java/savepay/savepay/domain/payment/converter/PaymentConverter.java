bea2da1339d6c6590bead9fa30a7eed99f5931ec
Hyun-38 <155708154+Hyun-38@users.noreply.github.com>
2025-07-26 11:31:21 +0900

    Feat(#10) : PaymentConverter 추가

---
+++ src/main/java/savepay/savepay/domain/payment/converter/PaymentConverter.java
@@ -0,0 +1,27 @@
+package savepay.savepay.domain.payment.converter;
+
+import savepay.savepay.domain.payment.dto.PaymentRequestDto;
+import savepay.savepay.domain.payment.dto.PaymentResponseDto;
+import savepay.savepay.domain.payment.dto.entity.Payment;
+
+public class PaymentConverter {
+
+    public static Payment toEntity(PaymentRequestDto dto) {
+        return Payment.builder()
+                .userId(dto.getUserId())
+                .cardId(dto.getCardId())
+                .amount(dto.getAmount())
+                .paymentType(dto.getPaymentType())
+                .build();
+    }
+
+    public static PaymentResponseDto toDto(Payment entity) {
+        return PaymentResponseDto.builder()
+                .id(entity.getId())
+                .userId(entity.getUserId())
+                .amount(entity.getAmount())
+                .paymentType(entity.getPaymentType())
+                .createdAt(entity.getCreatedAt())
+                .build();
+    }
+}
