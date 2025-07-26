3eef50285a09ed05277b2e1d3a998bd80262a902
Hyun-38 <155708154+Hyun-38@users.noreply.github.com>
2025-07-26 17:33:33 +0900

    Feat(#11) : PaymentController 개행 삭제 및 Apiresponse 추가

--- src/main/java/savepay/savepay/domain/payment/controller/PaymentController.java
+++ src/main/java/savepay/savepay/domain/payment/controller/PaymentController.java
@@ -4,16 +4,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import savepay.savepay.domain.payment.dto.PaymentResponseDto;
 import savepay.savepay.domain.payment.service.PaymentService;
+import savepay.savepay.global.ApiResponse;
 
 @RestController
 @RequestMapping("/payments")
 @RequiredArgsConstructor
-
 public class PaymentController {
     private final PaymentService paymentService;
 
     @GetMapping("/{cardId}")
-    public PaymentResponseDto getPayment(@PathVariable Long cardId) {
-        return paymentService.getPayment(cardId);
+    public ApiResponse<PaymentResponseDto> getPayment(@PathVariable Long cardId) {
+        PaymentResponseDto payment = paymentService.getPayment(cardId);
+        return ApiResponse.onSuccess(payment);
     }
 }
