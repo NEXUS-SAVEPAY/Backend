83c3cc35fd71a76c3fb432458d36a4d822bb0eac
Hyun-38 <155708154+Hyun-38@users.noreply.github.com>
2025-07-26 16:18:05 +0900

    Feat(#11) : UserPaymentService 추가

---
+++ src/main/java/savepay/savepay/domain/userpayment/service/UserPaymentService.java
@@ -0,0 +1,42 @@
+package savepay.savepay.domain.userpayment.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import savepay.savepay.domain.userpayment.converter.*;
+import savepay.savepay.domain.payment.entity.Payment;
+import savepay.savepay.domain.userpayment.dto.UserPaymentRequestDto;
+import savepay.savepay.domain.userpayment.dto.UserPaymentResponseDto;
+import savepay.savepay.domain.userpayment.entity.UserPayment;
+import savepay.savepay.domain.payment.repository.PaymentRepository;
+import savepay.savepay.domain.userpayment.repository.UserPaymentRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class UserPaymentService {
+
+    private final UserPaymentRepository userPaymentRepository;
+    private final PaymentRepository paymentRepository;
+
+    @Transactional
+    public UserPaymentResponseDto registerUserPayment(UserPaymentRequestDto dto) {
+        Payment payment = paymentRepository.findById(dto.getPaymentId())
+                .orElseThrow(() -> new IllegalArgumentException("결제수단 없음"));
+
+        UserPayment userPayment = UserPayment.builder()
+                .userId(dto.getUserId())
+                .payment(payment)
+                .build();
+        UserPayment saved = userPaymentRepository.save(userPayment);
+        return UserPaymentConverter.toDto(saved);
+    }
+
+    @Transactional(readOnly = true)
+    public List<UserPaymentResponseDto> getUserPayments(Long userId) {
+        List<UserPayment> list = userPaymentRepository.findByUserId(userId);
+        return list.stream().map(UserPaymentConverter::toDto).collect(Collectors.toList());
+    }
+}
