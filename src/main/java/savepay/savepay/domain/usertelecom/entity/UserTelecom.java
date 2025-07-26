fb2306748b42f8d1045fe45fc70835e49b4d771e
wnd01jun <changjun157@naver.com>
2025-07-26 15:18:05 +0900

    feat(#8): UserTelecom Entity 추가

---
+++ src/main/java/savepay/savepay/domain/usertelecom/entity/UserTelecom.java
@@ -0,0 +1,36 @@
+package savepay.savepay.domain.usertelecom.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import savepay.savepay.domain.common.BaseEntity;
+import savepay.savepay.domain.telecom.entity.Telecom;
+import savepay.savepay.domain.user.entity.User;
+
+@Entity
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class UserTelecom extends BaseEntity {
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "telecom_id")
+    private Telecom telecom;
+
+
+    private Boolean isMemberShip;
+
+    @Enumerated(EnumType.STRING)
+    private TelecomGrade grade;
+
+    public static UserTelecom createUserTelecom(User user, Telecom telecom,
+                                                TelecomGrade telecomGrade, Boolean isMemberShip) {
+        return new UserTelecom(user, telecom, isMemberShip, telecomGrade);
+    }
+}
