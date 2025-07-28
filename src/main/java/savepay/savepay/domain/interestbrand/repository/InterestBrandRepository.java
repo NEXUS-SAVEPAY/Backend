422c244d243472d7979e07813b6cad7634fb143e
jihoo2002 <yolohoo@naver.com>
2025-07-28 15:16:29 +0900

    Feat(#9) : InterestBrandRepository에 findByUser 추가

--- src/main/java/savepay/savepay/domain/interestbrand/repository/InterestBrandRepository.java
+++ src/main/java/savepay/savepay/domain/interestbrand/repository/InterestBrandRepository.java
@@ -2,6 +2,10 @@ package savepay.savepay.domain.interestbrand.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import savepay.savepay.domain.interestbrand.entity.InterestBrand;
+import savepay.savepay.domain.user.entity.User;
+
+import java.util.List;
 
 public interface InterestBrandRepository extends JpaRepository<InterestBrand, Long> {
+    List<InterestBrand> findByUser(User user);
 }
