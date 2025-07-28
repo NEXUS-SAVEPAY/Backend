fffeb6914ba6560161f3fe2de4c3bb5cefe57978
jihoo2002 <yolohoo@naver.com>
2025-07-28 15:19:28 +0900

    Feat(#9) : 엔터티에 Builder 및 Getter, NoArgsConstructor 아노테이션 추가

--- src/main/java/savepay/savepay/domain/interestbrand/entity/InterestBrand.java
+++ src/main/java/savepay/savepay/domain/interestbrand/entity/InterestBrand.java
@@ -4,11 +4,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.*;
+import org.springframework.http.HttpStatus;
 import savepay.savepay.domain.brand.entity.Brand;
 import savepay.savepay.domain.common.BaseEntity;
 import savepay.savepay.domain.user.entity.User;
 
 @Entity
+@Getter
+@NoArgsConstructor
 public class InterestBrand extends BaseEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -18,4 +22,10 @@ public class InterestBrand extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "brand_id")
     private Brand brand;
+
+    @Builder
+    public InterestBrand(User user, Brand brand) {
+        this.user = user;
+        this.brand = brand;
+    }
 }
