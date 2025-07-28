1a778b1f0b994f0210c2041012b37f1ca6b5056f
jihoo2002 <yolohoo@naver.com>
2025-07-28 15:15:51 +0900

    Feat(#9) : 유저의 관심브랜드 등록 및 삭제, 리스트 반환 구현

--- src/main/java/savepay/savepay/domain/interestbrand/controller/InterestBrandController.java
+++ src/main/java/savepay/savepay/domain/interestbrand/controller/InterestBrandController.java
@@ -2,13 +2,13 @@ package savepay.savepay.domain.interestbrand.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import savepay.savepay.domain.interestbrand.dto.InterestBrandRequestDto;
+import savepay.savepay.domain.interestbrand.dto.InterestBrandResponseDto;
 import savepay.savepay.domain.interestbrand.service.InterestBrandService;
 import savepay.savepay.global.ApiResponse;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,9 +18,24 @@ public class InterestBrandController {
     private final InterestBrandService interestBrandService;
 
     @PostMapping("/")
-    public ApiResponse<?> createInterestBrand(Authentication authentication,
-                                              @RequestBody InterestBrandRequestDto.toBrandIdDto request) {
+    public ApiResponse<String> createInterestBrand(Authentication authentication,
+                                                   @RequestBody InterestBrandRequestDto.toBrandIdDto request) {
         String email = authentication.getName();
         interestBrandService.createInterestBrand(email, request);
+        return ApiResponse.onSuccess("Successfully created interest brand");
+    }
+
+    @DeleteMapping("/")
+    public ApiResponse<String> deleteInterestBrand(Authentication authentication,
+                                                   @RequestBody InterestBrandRequestDto.toInterestBrandIdDto request) {
+        String email = authentication.getName();
+        interestBrandService.deleteInterestBrand(email, request);
+        return ApiResponse.onSuccess("Successfully deleted interest brand");
+    }
+
+    @GetMapping("/")
+    public ApiResponse<List<InterestBrandResponseDto.InterestBrandInfo>> getInterestBrands(Authentication authentication) {
+        String email = authentication.getName();
+        return ApiResponse.onSuccess(interestBrandService.getInterestBrands(email));
     }
 }
