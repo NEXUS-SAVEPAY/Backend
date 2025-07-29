4da471b30867ba067e5c2afd37be59ff71686bc2
jihoo2002 <yolohoo@naver.com>
2025-07-29 14:43:08 +0900

    Feat(#9) : 스웨거 태그 추가 및 검색 기록 API 추가

--- src/main/java/savepay/savepay/domain/interestbrand/controller/InterestBrandController.java
+++ src/main/java/savepay/savepay/domain/interestbrand/controller/InterestBrandController.java
@@ -1,5 +1,7 @@
 package savepay.savepay.domain.interestbrand.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -13,11 +15,13 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/interest/brand")
+@Tag(name = "관심 브랜드 API", description = "유저의 관심 브랜드 API")
 public class InterestBrandController {
 
     private final InterestBrandService interestBrandService;
 
     @PostMapping("/")
+    @Operation(summary = "유저의 관심 브랜드 등록 API", description = "유저의 관심 브랜드 등록하는 메서드")
     public ApiResponse<String> createInterestBrand(Authentication authentication,
                                                    @RequestBody InterestBrandRequestDto.toBrandIdDto request) {
         String email = authentication.getName();
@@ -26,6 +30,7 @@ public class InterestBrandController {
     }
 
     @DeleteMapping("/")
+    @Operation(summary = "유저의 관심 브랜드 삭제 API", description = "유저의 관심 브랜드 삭제하는 메서드")
     public ApiResponse<String> deleteInterestBrand(Authentication authentication,
                                                    @RequestBody InterestBrandRequestDto.toInterestBrandIdDto request) {
         String email = authentication.getName();
@@ -34,8 +39,25 @@ public class InterestBrandController {
     }
 
     @GetMapping("/")
+    @Operation(summary = "유저의 관심 브랜드 리스트 반환 API", description = "유저의 관심 브랜드 리스트를 반환하는 메서드")
     public ApiResponse<List<InterestBrandResponseDto.InterestBrandInfo>> getInterestBrands(Authentication authentication) {
         String email = authentication.getName();
         return ApiResponse.onSuccess(interestBrandService.getInterestBrands(email));
+    }
+
+    @PostMapping("/search")
+    @Operation(summary = "유저의 검색한 브랜드 저장 API", description = "유저가 검색한 브랜드를 저장하는 메서드")
+    public ApiResponse<String> createSearchBrands(Authentication authentication,
+                                                  @RequestBody InterestBrandRequestDto.toBrandIdDto request) {
+        String email = authentication.getName();
+        interestBrandService.createSearchBrands(email, request);
+        return ApiResponse.onSuccess("Successfully created search brand");
+    }
+
+    @GetMapping("/search")
+    @Operation(summary = "유저의 브랜드 검색어 리스트 API", description = "유저가 검색한 브랜드 리스트 내역을 반환하는 메서드")
+    public ApiResponse<List<InterestBrandResponseDto.InterestBrandInfo>> getSearchBrands(Authentication authentication) {
+        String email = authentication.getName();
+        return ApiResponse.onSuccess(interestBrandService.getSearchBrands(email));
     }
 }
