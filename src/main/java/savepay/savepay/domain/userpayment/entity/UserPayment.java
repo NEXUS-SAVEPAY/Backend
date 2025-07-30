1c32d2d31b869fdefef3ff82513c9dadb1cb7127
Hyun-38 <155708154+Hyun-38@users.noreply.github.com>
2025-07-30 12:33:58 +0900

    Chore(#11) : UserPayment 내 변수 이름 변경

--- src/main/java/savepay/savepay/domain/userpayment/entity/UserPayment.java
+++ src/main/java/savepay/savepay/domain/userpayment/entity/UserPayment.java
@@ -3,8 +3,6 @@ package savepay.savepay.domain.userpayment.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import savepay.savepay.domain.common.BaseEntity;
-import savepay.savepay.domain.payment.entity.Payment;
-import savepay.savepay.domain.user.entity.User;
 
 @Entity
 @Getter
@@ -15,10 +13,9 @@ import savepay.savepay.domain.user.entity.User;
 public class UserPayment extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
-    private User user;
+    private Long userId;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "payment_id")
-    private Payment payment;
-
+    private Long paymentId;
 }
