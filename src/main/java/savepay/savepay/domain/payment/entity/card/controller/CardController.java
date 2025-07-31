73aa313ca662318b7a04535383a43d877b3d7ee4
wnd01jun <changjun157@naver.com>
2025-07-31 16:30:31 +0900

    feat(#11): CardController 추가

---
+++ src/main/java/savepay/savepay/domain/payment/entity/card/controller/CardController.java
@@ -0,0 +1,57 @@
+package savepay.savepay.domain.payment.entity.card.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import savepay.savepay.domain.payment.entity.card.dto.CardRequestDto;
+import savepay.savepay.domain.payment.entity.card.dto.CardResponseDto;
+import savepay.savepay.domain.payment.entity.card.service.CardService;
+import savepay.savepay.domain.payment.service.UserCardService;
+import savepay.savepay.domain.user.entity.User;
+import savepay.savepay.global.ApiResponse;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cards")
+@RequiredArgsConstructor
+public class CardController {
+
+    private final CardService cardService;
+
+    private final UserCardService userCardService;
+
+    @PostMapping("/admin")
+    public ApiResponse<CardResponseDto> postCard(@RequestBody CardRequestDto.CardRegisterDto cardRegisterDto) {
+        return ApiResponse.onSuccess(cardService.registerCard(cardRegisterDto));
+    }
+
+    @DeleteMapping("/admin")
+    public ApiResponse<String> deleteCard(@RequestParam Long cardId) {
+        cardService.deleteCard(cardId);
+
+        return ApiResponse.onSuccess("Card is deleted!!");
+    }
+
+    @GetMapping("/list")
+    public ApiResponse<List<CardResponseDto>> findCardList(User user) {
+        return ApiResponse.onSuccess(userCardService.findUserCardList(user));
+    }
+
+    @GetMapping("/search")
+    public ApiResponse<CardResponseDto> searchCard(CardRequestDto.CardSearchDto cardSearchDto) {
+        return ApiResponse.onSuccess(cardService.searchCard(cardSearchDto));
+    }
+
+    @PostMapping("/user")
+    public ApiResponse<String> postCard(@RequestParam(name = "cardId")Long cardId, User user) {
+        userCardService.registerUserCard(cardId, user);
+        return ApiResponse.onSuccess("User's card is successfully registered");
+    }
+
+    @DeleteMapping("/user")
+    public ApiResponse<String> deleteCard(@RequestParam(name = "cardId")Long cardId, User user) {
+        userCardService.deleteUserCard(cardId, user);
+        return ApiResponse.onSuccess("User's card is successfully deleted");
+    }
+
+}
