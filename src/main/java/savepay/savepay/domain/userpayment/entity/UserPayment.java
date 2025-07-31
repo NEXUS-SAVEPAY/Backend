a7d53d9e5c672c7e872ed400e085b1f2f3df57de
wnd01jun <changjun157@naver.com>
2025-07-31 16:33:53 +0900

    refactor(#11): UserPayment 필드 수정

--- src/main/java/savepay/savepay/domain/userpayment/entity/UserPayment.java
+++ src/main/java/savepay/savepay/domain/userpayment/entity/UserPayment.java
@@ -3,6 +3,8 @@ package savepay.savepay.domain.userpayment.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import savepay.savepay.domain.common.BaseEntity;
+import savepay.savepay.domain.payment.entity.Payment;
+import savepay.savepay.domain.user.entity.User;
 
 @Entity
 @Getter
@@ -13,9 +15,17 @@ import savepay.savepay.domain.common.BaseEntity;
 public class UserPayment extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
-    private Long userId;
+    private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "payment_id")
-    private Long paymentId;
+    private Payment payment;
+
+    // 멤버쉽을 payment별로 생성해서 중복 레코드 생성보다 이 구조가 나을 것 같습니다.
+    private Boolean isMembership;
+
+    public static UserPayment createUserPayment(User user, Payment payment, Boolean isMembership) {
+        return new UserPayment(user, payment, isMembership);
+    }
+
 }
