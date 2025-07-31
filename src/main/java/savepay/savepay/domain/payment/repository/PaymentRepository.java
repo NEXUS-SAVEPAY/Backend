3af175404783d1ba8edeb68eb1e407e09a77d279
wnd01jun <changjun157@naver.com>
2025-07-31 16:30:09 +0900

    chore(#11): 패키지 위치 이동

--- src/main/java/savepay/savepay/domain/payment/repository/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package savepay.savepay.domain.payment.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import savepay.savepay.domain.payment.entity.Payment;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
