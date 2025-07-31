6083cd206cf032541baf97bcbadadcddb4b9ce77
wnd01jun <changjun157@naver.com>
2025-07-31 16:32:17 +0900

    feat(#11): ErrorStatus Card관련 ErrorStatus 추가

--- src/main/java/savepay/savepay/global/code/status/ErrorStatus.java
+++ src/main/java/savepay/savepay/global/code/status/ErrorStatus.java
@@ -13,7 +13,9 @@ public enum ErrorStatus implements BaseErrorCode {
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON400", "잘못된 요청입니다."),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON401", "인증이 필요합니다."),
     PAYMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "PAYMENT404", "결제 수단을 찾을 수 없습니다."),
-    FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다.");
+    FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
+    CARD_NOT_FOUND(HttpStatus.NOT_FOUND, "CARD404", "카드를 찾을 수 없습니다."),
+    USER_PAYMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_PAYMENT404", "사용자 결제수단을 찾을 수 없습니다.");
 
 
     private final HttpStatus httpStatus;
