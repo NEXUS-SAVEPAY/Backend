4293b97fcb795ea4981f877ee6c77cfef05cc745
wnd01jun <changjun157@naver.com>
2025-07-31 16:31:10 +0900

    feat(#11): CardRepository 추가

---
+++ src/main/java/savepay/savepay/domain/payment/entity/card/repository/CardRepository.java
@@ -0,0 +1,21 @@
+package savepay.savepay.domain.payment.entity.card.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import savepay.savepay.domain.payment.entity.card.entity.Card;
+
+import java.util.Optional;
+
+public interface CardRepository extends JpaRepository<Card, Long> {
+
+    @Query("""
+        SELECT c FROM Card c
+            WHERE c.company = :company
+            AND c.cardName LIKE CONCAT('%', :cardName, '%')
+            LIMIT 1
+
+""")
+    Optional<Card> searchCard(@Param("card")String company, @Param("cardName")String cardName);
+
+}
