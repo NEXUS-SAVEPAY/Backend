cbc46a570fe3b5223b4380fd6cb380664ed8f47c
wnd01jun <changjun157@naver.com>
2025-08-07 14:49:00 +0900

    feat(#14): CustomUserDetailsService 추가

---
+++ src/main/java/savepay/savepay/global/security/service/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package savepay.savepay.global.security.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import savepay.savepay.domain.user.repository.UserRepository;
+import savepay.savepay.global.security.domain.CustomUserDetails;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    /**
+     * 유저의 email을 기반으로 user를 찾는다.
+     * @param username
+     * @return
+     * @throws UsernameNotFoundException
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .map(CustomUserDetails::new)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("해당 유저는 찾을 수 없습니다.")
+                );
+    }
+}
