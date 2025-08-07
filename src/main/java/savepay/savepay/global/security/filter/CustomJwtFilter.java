0ec7ac3d8a215d0afc39a478f65b546e964fbd25
wnd01jun <changjun157@naver.com>
2025-08-07 14:49:29 +0900

    feat(#14): JWT Filter 추가

---
+++ src/main/java/savepay/savepay/global/security/filter/CustomJwtFilter.java
@@ -0,0 +1,35 @@
+package savepay.savepay.global.security.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import savepay.savepay.global.security.JwtTokenProvider;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class CustomJwtFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String token = jwtTokenProvider.resolveToken(request);
+
+        if (token != null && jwtTokenProvider.validateToken(token)) {
+            UsernamePasswordAuthenticationToken authenticationToken = jwtTokenProvider.getAuthenticationToken(token);
+
+            SecurityContextHolder.getContextHolderStrategy().getContext().setAuthentication(authenticationToken);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
