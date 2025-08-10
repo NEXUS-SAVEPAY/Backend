539cd9f8d4ab43217b1a8b1a9339c7eb11a90c5b
wnd01jun <changjun157@naver.com>
2025-08-10 18:49:44 +0900

    refactor(#14): TokenService 토큰 관련 책임 위임

---
+++ src/main/java/savepay/savepay/global/security/domain/service/TokenService.java
@@ -0,0 +1,87 @@
+package savepay.savepay.global.security.domain.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import savepay.savepay.domain.user.entity.User;
+import savepay.savepay.domain.user.repository.UserRepository;
+import savepay.savepay.global.code.status.ErrorStatus;
+import savepay.savepay.global.exception.GeneralException;
+import savepay.savepay.global.security.JwtTokenProvider;
+import savepay.savepay.global.security.domain.RefreshToken;
+import savepay.savepay.global.security.domain.repository.RefreshTokenRepository;
+import savepay.savepay.global.security.service.CustomUserDetailsService;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class TokenService {
+
+    private final UserRepository userRepository;
+
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    private final CustomUserDetailsService customUserDetailsService;
+
+    public boolean validateRefreshToken(String refreshToken) {
+        if (!jwtTokenProvider.isRefresh(refreshToken) || !jwtTokenProvider.validateToken(refreshToken)) {
+            throw new GeneralException(ErrorStatus.INVALID_TOKEN);
+        }
+
+        String email = jwtTokenProvider.getUsernameFromToken(refreshToken);
+
+        User user = userRepository.findByEmail(email).orElseThrow(() ->
+                new GeneralException(ErrorStatus.USER_NOT_FOUND)
+        );
+        Optional<RefreshToken> optionalRefreshToken = refreshTokenRepository.findByUser(user);
+
+        // Refresh hash적용 가능성 생각해보기
+        return optionalRefreshToken.map(token ->
+                token.getRefreshToken().equals(refreshToken)).orElse(false);
+    }
+
+    public boolean validateAccessToken(String accessToken) {
+        return jwtTokenProvider.validateToken(accessToken) && jwtTokenProvider.isAccess(accessToken);
+    }
+
+    public String generateAccessToken(String email) {
+        return jwtTokenProvider.generateAccessToken(email);
+    }
+
+    public UsernamePasswordAuthenticationToken getAuthenticationToken(String token) {
+        if (!validateAccessToken(token)) {
+            throw new GeneralException(ErrorStatus.INVALID_TOKEN);
+        }
+        String email = jwtTokenProvider.getUsernameFromToken(token);
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername(email);
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+    }
+
+
+    public String reissueAccessToken(String refreshToken) {
+        if (!validateRefreshToken(refreshToken)) {
+            throw new GeneralException(ErrorStatus.INVALID_TOKEN);
+        }
+        return jwtTokenProvider.generateAccessToken(jwtTokenProvider.getUsernameFromToken(refreshToken));
+    }
+
+    public String generateRefreshToken(String email) {
+        String refreshToken = jwtTokenProvider.generateRefreshToken(email);
+        User user = userRepository.findByEmail(email).orElseThrow(() ->
+                new GeneralException(ErrorStatus.USER_NOT_FOUND));
+
+        refreshTokenRepository.findByUser(user).ifPresent(refreshTokenRepository::delete);
+
+        RefreshToken refreshTokenObject = RefreshToken.createRefreshToken(user, refreshToken);
+        refreshTokenRepository.save(refreshTokenObject);
+
+        return refreshToken;
+    }
+
+}
