0357eac0908a0cb925cc05bb3c1b0a9bb0de2e46
wnd01jun <changjun157@naver.com>
2025-08-10 18:50:02 +0900

    feat(#14): JwtExceptionFilter 추가

---
+++ src/main/java/savepay/savepay/global/security/filter/JwtExceptionFilter.java
@@ -0,0 +1,55 @@
+package savepay.savepay.global.security.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import savepay.savepay.global.ApiResponse;
+import savepay.savepay.global.exception.GeneralException;
+
+import java.io.IOException;
+import java.nio.file.AccessDeniedException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtExceptionFilter extends OncePerRequestFilter {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
+            throws ServletException, IOException {
+        try {
+            chain.doFilter(req, res);
+        } catch (BadCredentialsException ex) {
+            writeDefault(res);
+            ApiResponse<Object> body = ApiResponse.onFailure(HttpStatus.UNAUTHORIZED.toString(), ex.getMessage(), null);
+            writeBody(res, HttpStatus.UNAUTHORIZED.toString(), body);
+        } catch (AccessDeniedException ex) {
+            writeDefault(res);
+            ApiResponse<Object> body = ApiResponse.onFailure(HttpStatus.FORBIDDEN.toString(), ex.getMessage(), null);
+            writeBody(res, HttpStatus.FORBIDDEN.toString(), body);
+        } catch (GeneralException ex) {
+            writeDefault(res);
+            ApiResponse<Object> body = ApiResponse.onFailure(ex.getErrorReasonHttpStatus().getCode(), ex.getErrorReasonHttpStatus().getMessage(), null);
+            writeBody(res, ex.getErrorReasonHttpStatus().getCode(), body);
+        }
+    }
+
+    private void writeDefault(HttpServletResponse res) {
+        res.setContentType("application/json;charset=UTF-8");
+    }
+
+    private void writeBody(HttpServletResponse res, String status, ApiResponse<Object> body) throws IOException {
+        res.setStatus(Integer.getInteger(status));
+        String json = objectMapper.writeValueAsString(body);
+        res.getWriter().write(json);
+    }
+
+}
