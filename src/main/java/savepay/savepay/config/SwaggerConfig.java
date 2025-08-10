02c963adf06a256772a6808a1a5e22af07fb3fb9
wnd01jun <changjun157@naver.com>
2025-08-10 19:41:35 +0900

    refactor(#14): Swagger JWT Token 전송 가능하도록 변경

--- src/main/java/savepay/savepay/config/SwaggerConfig.java
+++ src/main/java/savepay/savepay/config/SwaggerConfig.java
@@ -3,15 +3,29 @@ package savepay.savepay.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    private static final String BEARER_KEY = "bearerAuth";
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components()
+                        .addSecuritySchemes(BEARER_KEY,
+                                new SecurityScheme()
+                                        .name(BEARER_KEY)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT") // UI에 표기용
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement().addList(BEARER_KEY))
                 .info(apiInfo());
     }
 
@@ -19,6 +33,6 @@ public class SwaggerConfig {
         return new Info()
                 .title("SavePay")
                 .description("세이브페이 스웨거입니다.")
-                .version("1.0.0");
+                .version("1.0.1");
     }
 }
