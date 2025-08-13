0a43d64ecab4d7452e6956a186b69fd070b1e184
wnd01jun <changjun157@naver.com>
2025-08-13 20:42:53 +0900

    refactor(#26): UserInjection Swagger에서 User 안보이도록 숨김

--- src/main/java/savepay/savepay/global/security/resolver/UserInjection.java
+++ src/main/java/savepay/savepay/global/security/resolver/UserInjection.java
@@ -1,9 +1,12 @@
 package savepay.savepay.global.security.resolver;
 
+import io.swagger.v3.oas.annotations.Hidden;
+
 import java.lang.annotation.*;
 
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
+@Hidden
 public @interface UserInjection {
 }
