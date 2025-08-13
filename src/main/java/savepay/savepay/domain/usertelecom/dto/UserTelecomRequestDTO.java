4905be858900f61e33ee56523ec71a8e7321e911
wnd01jun <changjun157@naver.com>
2025-08-13 20:43:24 +0900

    refactor(#26): UserTelecom Enum 주입받음에 따라 관련된 사항들 변경

--- src/main/java/savepay/savepay/domain/usertelecom/dto/UserTelecomRequestDTO.java
+++ src/main/java/savepay/savepay/domain/usertelecom/dto/UserTelecomRequestDTO.java
@@ -2,12 +2,13 @@ package savepay.savepay.domain.usertelecom.dto;
 
 import lombok.Builder;
 import savepay.savepay.domain.telecom.entity.TelecomName;
+import savepay.savepay.domain.usertelecom.entity.TelecomGrade;
 
 @Builder
 public record UserTelecomRequestDTO(
         TelecomName telecomName,
         Boolean isMemberShip,
-        String grade
+        TelecomGrade grade
 
 ) {
 }
