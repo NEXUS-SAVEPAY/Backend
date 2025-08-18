c61f9a6cb897f6d7f19f0433505e4ce729bc7eb3
wnd01jun <changjun157@naver.com>
2025-08-18 16:40:32 +0900

    feat(#39): CardService 카드 이미지 로직 수정 및 admin 로직 추가

--- src/main/java/savepay/savepay/domain/payment/entity/card/service/CardService.java
+++ src/main/java/savepay/savepay/domain/payment/entity/card/service/CardService.java
@@ -3,13 +3,18 @@ package savepay.savepay.domain.payment.entity.card.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 import savepay.savepay.domain.payment.entity.card.converter.CardConverter;
 import savepay.savepay.domain.payment.entity.card.dto.CardRequestDto;
 import savepay.savepay.domain.payment.entity.card.dto.CardResponseDto;
 import savepay.savepay.domain.payment.entity.card.entity.Card;
 import savepay.savepay.domain.payment.entity.card.repository.CardRepository;
+import savepay.savepay.global.aws.AwsS3Service;
 import savepay.savepay.global.code.status.ErrorStatus;
 import savepay.savepay.global.exception.GeneralException;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 카드 자체적인 기능만 담당합니다.
@@ -22,6 +27,8 @@ public class CardService {
 
     private final CardRepository cardRepository;
 
+    private final AwsS3Service awsS3Service;
+
     public CardResponseDto searchCard(CardRequestDto.CardSearchDto cardSearchDto) {
         Card card = cardRepository.findFirstByCompanyAndCardNameContaining(cardSearchDto.company(), cardSearchDto.cardName())
                 .orElseThrow(
@@ -31,8 +38,10 @@ public class CardService {
     }
 
     @Transactional
-    public CardResponseDto registerCard(CardRequestDto.CardRegisterDto cardRegisterDto) {
-        Card card = CardConverter.toEntity(cardRegisterDto);
+    public CardResponseDto registerCard(MultipartFile multipartFile, CardRequestDto.CardRegisterDto cardRegisterDto) {
+        String image = awsS3Service.uploadFile(multipartFile);
+
+        Card card = CardConverter.toEntity(cardRegisterDto, image);
         cardRepository.save(card);
 
         return CardConverter.toDto(card);
@@ -44,5 +53,16 @@ public class CardService {
                 () -> new GeneralException(ErrorStatus.CARD_NOT_FOUND));
 
         cardRepository.delete(card);
+    }
+
+    public CardResponseDto findById(Long cardId) {
+        return CardConverter.toDto(cardRepository.findById(cardId).orElseThrow(() ->
+                new GeneralException(ErrorStatus.CARD_NOT_FOUND)));
+    }
+
+    public List<CardResponseDto> findAll() {
+        return cardRepository.findAll().stream()
+                .map(CardConverter::toDto)
+                .collect(Collectors.toList());
     }
 }
